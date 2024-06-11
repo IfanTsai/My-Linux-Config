fb89591e7865431f3ff9d9dd958be853d77da36e
IfanTsai <i@caiyifan.cn>
2024-06-11 22:25:18 +0800

    format rust code on save

--- nvim/lua/usr/autocmd.lua
+++ nvim/lua/usr/autocmd.lua
@@ -8,10 +8,18 @@ vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
 
 local format_sync_group = vim.api.nvim_create_augroup("format_sync", {})
 
-vim.api.nvim_create_autocmd("BufWritePre", {
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   pattern = "*.go",
   callback = function()
     require('go.format').goimport()
+  end,
+  group = format_sync_group,
+})
+
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  pattern = { '*.rs' },
+  callback = function()
+    vim.lsp.buf.format{ async = true }
   end,
   group = format_sync_group,
 })
